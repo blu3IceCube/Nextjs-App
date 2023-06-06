39e8557cb237a9dbb99f570e8199f4331ab18d73
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2023-06-06 22:00:14 +0530

    fetching local data

--- app/portfolio/[category]/page.jsx
+++ app/portfolio/[category]/page.jsx
@@ -1,30 +1,35 @@
 import Button from "@/components/button/button";
 import Image from "next/image";
+import { items } from "./data";
+import { notFound } from "next/navigation";
 
-export default function Category({params}) {
+const getData = (category) => {
+    const data = items[category]
+
+    if (data) {
+        return data
+    }
+
+    return notFound()
+}
+
+export default function Category({ params }) {
+    const data = getData(params.category)
     return (
         <div>
             <h1 className="text-green-400 text-4xl font-medium">{params.category}</h1>
-            <div className="flex gap-12 mt-12 mb-24 odd:flex-row-reverse">
-                <div className="flex flex-col gap-5 flex-1 justify-center">
-                    <h1 className="text-5xl font-medium">Creative Portfolio</h1>
-                    <p className="text-xl font-light">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?</p>
-                    <Button url='#' text='See More'/>
+            {data.map(item => (
+                <div className="flex gap-12 mt-12 mb-24 odd:flex-row-reverse" key={item.id}>
+                    <div className="flex flex-col gap-5 flex-1 justify-center">
+                        <h1 className="text-5xl font-medium">{item.title}</h1>
+                        <p className="text-xl font-light">{item.desc}</p>
+                        <Button url='#' text='See More' />
+                    </div>
+                    <div className="flex-1 relative h-75">
+                        <Image className="object-cover" src={item.image} fill={true} alt='' />
+                    </div>
                 </div>
-                <div className="flex-1 relative h-75">
-                    <Image className="object-cover" src='https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg' fill={true} alt=''/>
-                </div>
-            </div>
-            <div className="flex gap-12 mt-12 mb-24 odd:flex-row-reverse">
-                <div className="flex flex-col gap-5 flex-1 justify-center">
-                    <h1 className="text-5xl font-medium">Creative Portfolio</h1>
-                    <p className="text-xl font-light">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?</p>
-                    <Button url='#' text='See More'/>
-                </div>
-                <div className="flex-1 relative h-75">
-                    <Image className="object-cover" src='https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg' fill={true} alt=''/>
-                </div>
-            </div>
+            ))}
         </div>
     )
 }
