df2d337ee12da215b2ccc171594de8855fb91dc0
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2023-06-19 15:25:35 +0530

    adding post creation/deletion form in dashboard

--- utils/db.js
+++ utils/db.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 export default async function connect() {
   try {
     await mongoose.connect(process.env.URI);
-    console.log("connection succesfull!");
   } catch (error) {
     throw new Error("Connection failed!");
   }
